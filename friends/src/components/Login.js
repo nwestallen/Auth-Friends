f7e7c507366aba90cb7a1eb3d36addcbb3ff98c5
Nick Allen <nwestallen@gmail.com>
2021-02-23 18:59:17 -0500

    successful login token

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 export default function Login() {
 
-    const [formData, setFormData] = useState({username: '', password: ''});
+    const [formData, setFormData] = useState({username: '', password: '', isLoading: false});
 
     const handleChange = e => {
         const { name, value } = e.target;
         setFormData({...formData, [name]: value});
     };
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        setFormData({...formData, isLoading: true});
+        axiosWithAuth()
+            .post('/api/login', formData)
+            .then(res => {
+                console.log(res);
+                localStorage.setItem('token', JSON.stringify(res.data.payload));
+            })
+            .catch(err => console.log({ err }));
+    };
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Login</h2>
                 <label>
                     Username:
@@ -21,6 +34,7 @@ export default function Login() {
                     Password:
                     <input type='password' name='password' value={formData.password} onChange={handleChange}/>
                 </label>
+                <button>Submit</button>
             </form>
         </div>
     )
