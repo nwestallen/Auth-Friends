6bdfccd5d1fca1edf25a139c13bf175d8df730b9
Nick Allen <nwestallen@gmail.com>
2021-02-23 21:15:58 -0500

    render Friend component for each friend

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import Friend from './Friend';
 
 export default function FriendList() {
+
+    const [friendData, setFriendData] = useState([{name: ''}]);
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get('/api/friends')
+        .then(res => {
+            console.log(res);
+            setFriendData(res.data);
+        })
+        .catch(err => console.log({ err }));
+    },[]);
+
     return (
         <div>
-           Here are your friends! 
+           Here are your friends!
+           {friendData.map(friend => <Friend name={friend.name} age={friend.age} email={friend.email} />)}
         </div>
     )
 };
